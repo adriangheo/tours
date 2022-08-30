983b4c9804fd8731cf4692572e79972e89aff363
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-30 20:50:07 +0300

    final version of code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from 'react'
+import Tours from './Tours'
+import data from './data.js'
+
 
 function App() {
+  const [tours, setTours] = useState(data)
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => {return data}}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: "rec6d6T5q5EBIdCfD",
+    name: "Best of Viena in 4 Days Tour",
+    info: "Vienna is the capital, largest city, and  Austria's most populous city, with about two million inhabitants. It is also the cultural, economic, and political center. Among the many tourist attractions of Vienna, one of the most important to see is the magnificent Schönbrunn Palace. The 1,441-room Rococo palace is one of the most important architectural, cultural, and historic monuments in the country. The history of the palace and its vast gardens spans over 300 years, reflecting the changing tastes, interests, and aspirations of successive Habsburg monarchs.  Join us for the Best of Paris in 7 Days!",
+    image:
+      "https://i.imgur.com/uqiufEe.png",
+    price: "1,395",
+  },
+  {
+    id: "recIwdrvU9HfJR3B4",
+    name: "Best of London in 5 Days",
+    info: "Everyone should visit London at least once in their life. Big Ben, the Tower of London, Buckingham Palace, the London Eye, the list goes on and on. There's nothing quite like the experience of hopping on a red double-decker bus and seeing some of the most recognisable sights in the world from its top deck.",
+    image:
+      "https://i.imgur.com/RYuvOSr.jpg",
+    price: "1,595",
+  },
+  {
+    id: "recJLXcHScdUtI3ny",
+    name: "Best of Athens in 3 Days",
+    info: "Athens boasts an astonishing blend of cultural attractions, historical monuments, exquisite gastronomy, marvelous hotels, amazing nightlife, and magnificent beaches. If you only visit one landmark during your stay, make it the Acropolis, an awe-inspiring ancient citadel which overlooks the city.",
+    image:
+      "https://i.imgur.com/uCD7u5G.jpg",
+    price: "1,195",
+  },
+  {
+    id: "recK3AOoVhIHPLUwn",
+    name: "Best of Copenhagen in 5 Days Tour",
+    info: " Danes rank among the world's most happy people. In Copenhagen and the beautiful surrounding region, you'll learn why. The buzzing capital mixes royal history, modern architecture and loads of culture with sustainable living and a mouthwatering restaurant scene.",
+    image:
+      "https://i.imgur.com/pi720q2.jpg",
+    price: "1,795",
+  },
+  {
+    id: "receABzz86KzW2gvH",
+    name: "Best of Berlin in 3 Days Tour",
+    info: "Berlin, the capital of Germany and the country's largest city, is also a major center of politics, culture, media, and science. Noted for its cultural flair, Berlin is home to the world famous Berlin Opera and Berlin Philharmonic Orchestra, while its diverse art scene encompasses hundreds of galleries, events, and museums, including those on Museum Island, a UNESCO World Heritage Site.",
+    image:
+      "https://i.imgur.com/molsokk.jpg",
+    price: "1,495",
+  },
+];
